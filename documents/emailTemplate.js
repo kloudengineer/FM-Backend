8192f4d6ff5059a59a4d7a3d159a74feb6db60b0
cabdiaziz <abdiaziiz1856@gmail.com>
2022-10-05 01:33:30 +0300

    updated staff api and refectoring the code also updated email messages and created a service folder for notifications

--- documents/emailTemplate.js
+++ documents/emailTemplate.js
@@ -97,14 +97,12 @@ exports.emailTemplate2 = (firstName, cardName, expDate, email, type, count) => {
                        count == 3
                          ? type === "Warning"
                            ? `Warning ${firstName}'s ${cardName} will expire after ${expDate}-month and it's email : ${email}\n
-                            this is the last notification about this event because you reached the limit \n
-                            Please click on the link below to take an action for this event <a href="http://localhost:">`
-                           : `Warning for inactivation we set ${firstName}'s status to -InActive- because it's ${cardName} expired 
-                            before ${expDate}-month ago and it's email : ${email} \n 
                             this is the last notification about this event because you reached the limit`
+                           : `Warning ${firstName}'s status is inactive and he cann't drive because it's ${cardName} expired 
+                            before ${expDate}-month ago and it's email : ${email} \n 
+                            and this is the last notification about this event because you reached the limit`
                          : type === "Warning"
-                         ? `Warning ${firstName}'s ${cardName} will expire after ${expDate}-month and it's email : ${email}\n
-                            Please click on the link below to take an action for this event <a href="http://localhost:">`
+                         ? `Warning ${firstName}'s ${cardName} will expire after ${expDate}-month and it's email : ${email}\n`
                          : `Warning for inactivation we set ${firstName}'s status to -InActive- because 
                             it's ${cardName} expired before ${expDate}-month ago and it's email : ${email} `
                      }</p> Fleet Management
